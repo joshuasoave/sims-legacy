cff68398f5c07687f85daea486080f44bc8d5d69
Joshua <joshua.soave@gmail.com>
2024-03-15 00:50:06 -0400

    img carousel for entries

--- src/Data/EntryData.js
+++ src/Data/EntryData.js
@@ -4,7 +4,23 @@ export const entryData = [
     date: "2024-03-14",
     title: "The Beginning",
     story:
-      "Today, I created my founder, a young adult named Axel. I'm excited to see where this goes.",
-    photo: "https://i.imgur.com/0x8Ij6q.jpg",
+      "My name is Axel Rosethorn. I'm a spellcaster moving to Del Sol Valley. Magic will guide me on my quest to become a celebrity. I'm excited to see where this goes.",
+    photo: "../Assets/Axel.png",
+  },
+  {
+    id: 2,
+    date: "2024-03-14",
+    title: "Meeting Bella",
+    story:
+      "I met Bella Goth at my new lot in Del Sol Valley. We hit it off when she gave me a rose. I even thought she might be the one. Until she revealed she's married to Mortimer Goth. I guess I'll have to find someone else.",
+    photo: "../Assets/Bella.png",
+  },
+  {
+    id: 3,
+    date: "2024-03-14",
+    title: "Singing at the Bar",
+    story:
+      "I went to the bar to sing and make some money. A lot of people showed up, but they had their backs to me. I think I'll come back once I've had more practice.",
+    photo: "../Assets/Singing.png",
   },
 ];
