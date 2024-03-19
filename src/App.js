aea37b9f385c2e11587e6b70c0451eefbb483647
Joshua <joshua.soave@gmail.com>
2024-03-19 16:50:43 -0400

    removing is mobile from app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Entries from "./Components/Entries";
 import ChallengeInfo from "./Components/ChallengeInfo";
@@ -7,71 +7,40 @@ import { CastInfo } from "./Components/CastInfo";
 
 function App() {
   const [activeTab, setActiveTab] = useState("entry");
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
 
   return (
     <div className="App">
-      {!isMobile && (
-        <>
-          <header className="App-header">
-            <h1>Sims 4 Road to Fame</h1>
-          </header>
-          <div className="Tabs">
-            <button
-              className={activeTab === "challenge" ? "active" : ""}
-              onClick={() => setActiveTab("challenge")}
-            >
-              About
-            </button>
+      <header className="App-header">
+        <h1>Sims 4 Road to Fame</h1>
+      </header>
+      <div className="Tabs">
+        <button
+          className={activeTab === "challenge" ? "active" : ""}
+          onClick={() => setActiveTab("challenge")}
+        >
+          About
+        </button>
 
-            <button
-              className={activeTab === "rules" ? "active" : ""}
-              onClick={() => setActiveTab("rules")}
-            >
-              Rules
-            </button>
-            <button
-              className={activeTab === "cast" ? "active" : ""}
-              onClick={() => setActiveTab("cast")}
-            >
-              Cast
-            </button>
-            <button
-              className={activeTab === "entry" ? "active" : ""}
-              onClick={() => setActiveTab("entry")}
-            >
-              Entries
-            </button>
-          </div>
-        </>
-      )}
+        <button
+          className={activeTab === "rules" ? "active" : ""}
+          onClick={() => setActiveTab("rules")}
+        >
+          Rules
+        </button>
+        <button
+          className={activeTab === "cast" ? "active" : ""}
+          onClick={() => setActiveTab("cast")}
+        >
+          Cast
+        </button>
+        <button
+          className={activeTab === "entry" ? "active" : ""}
+          onClick={() => setActiveTab("entry")}
+        >
+          Entries
+        </button>
+      </div>
       <div className="TabContent">
-        {isMobile ? (
-          <header className="App-header">
-            <h1>Sims 4 Road to Fame</h1>
-            <nav>
-              <button onClick={() => setActiveTab("entry")}>Entries</button>
-              <button onClick={() => setActiveTab("challenge")}>
-                Challenge Info
-              </button>
-              <button onClick={() => setActiveTab("rules")}>Rules</button>
-              <button onClick={() => setActiveTab("cast")}>Cast</button>
-              <button onClick={() => setActiveTab("about")}>About</button>
-            </nav>
-          </header>
-        ) : null}
         {activeTab === "entry" && <Entries />}
         {activeTab === "challenge" && <ChallengeInfo />}
         {activeTab === "rules" && <Rules />}
