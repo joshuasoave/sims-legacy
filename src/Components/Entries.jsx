1823672a15479e2e67dc03ed600085f52e4ca338
Joshua <joshua.soave@gmail.com>
2024-03-20 20:49:28 -0400

    fixing slideshow on entries

--- src/Components/Entries.jsx
+++ src/Components/Entries.jsx
@@ -1,25 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { entryData } from "../Data/EntryData";
 import ImageCarousel from "./ImageCarousel";
 
 const Entries = () => {
-  const [entryIndex, setEntryIndex] = useState(0);
+  const photoIndex = 0;
   const [expanded, setExpanded] = useState(false);
-
-  useEffect(() => {
-    const handleKeyDown = (e) => {
-      if (e.key === "ArrowRight" && entryIndex < entryData.length - 1) {
-        setEntryIndex(entryIndex + 1);
-      } else if (e.key === "ArrowLeft" && entryIndex > 0) {
-        setEntryIndex(entryIndex - 1);
-      }
-    };
-    window.addEventListener("keydown", handleKeyDown);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, [entryIndex]);
 
   const toggleExpand = () => {
     setExpanded(!expanded);
@@ -28,18 +13,10 @@ const Entries = () => {
   return (
     <div className="entryContentContainer">
       {entryData.map((entry, index) => (
-        <div
-          key={index}
-          className={`entry ${index === entryIndex ? "active" : ""}`}
-        >
+        <div key={index} className={`entry`}>
           <h2>{entry.title}</h2>
-          <ImageCarousel photos={entry.photos} />
-          <p
-            className={`entryStory ${
-              expanded && entryIndex === index ? "expanded" : ""
-            }`}
-            onClick={toggleExpand}
-          >
+          <ImageCarousel photos={entry.photos} prevPhotoIndex={photoIndex} />
+          <p className={`entryStory`} onClick={toggleExpand}>
             {entry.story}
           </p>
         </div>
--- src/Components/ImageCarousel.jsx
+++ src/Components/ImageCarousel.jsx
@@ -1,53 +1,65 @@
 import React, { useState, useEffect, useCallback } from "react";
 import PhotoModal from "./PhotoModal";
 
-const ImageCarousel = ({ photos = [] }) => {
-  const [photoIndex, setPhotoIndex] = useState(0);
+const ImageCarousel = ({ photos = [], prevPhotoIndex }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [photoIndex, setPhotoIndex] = useState(prevPhotoIndex);
+
   useEffect(() => {
     const interval = setInterval(() => {
+      if (!isModalOpen) {
+        if (photoIndex < photos.length - 1) {
+          setPhotoIndex(photoIndex + 1);
+        } else if (photoIndex === photos.length - 1) {
+          setPhotoIndex(0);
+        }
+      } else {
+        clearInterval(interval);
+      }
+    }, 3500);
+
+    return () => clearInterval(interval);
+  }, [isModalOpen, photoIndex, photos.length, setPhotoIndex]);
+
+  useEffect(() => {
+    setPhotoIndex(0);
+  }, [photos, setPhotoIndex]);
+
+  const increasePhotoIndex = useCallback(() => {
+    if (!isModalOpen) {
       if (photoIndex < photos.length - 1) {
         setPhotoIndex(photoIndex + 1);
       } else {
         setPhotoIndex(0);
       }
-    }, 3500);
-    return () => clearInterval(interval);
-  }, [photoIndex, photos]);
-
-  useEffect(() => {
-    setPhotoIndex(0);
-  }, [photos]);
-
-  const increasePhotoIndex = useCallback(() => {
-    if (photoIndex < photos.length - 1) {
-      setPhotoIndex(photoIndex + 1);
-    } else if (photoIndex === photos.length - 1) {
-      setPhotoIndex(0);
     }
-  }, [photoIndex, photos]);
+  }, [photoIndex, photos.length, setPhotoIndex, isModalOpen]);
 
   const decreasePhotoIndex = useCallback(() => {
-    if (photoIndex === 0) {
-      setPhotoIndex(photos.length - 1);
-    } else if (photoIndex > 0) {
-      setPhotoIndex(photoIndex - 1);
+    if (!isModalOpen) {
+      if (photoIndex === 0) {
+        setPhotoIndex(photos.length - 1);
+      } else {
+        setPhotoIndex(photoIndex - 1);
+      }
     }
-  }, [photoIndex, photos]);
+  }, [photoIndex, photos.length, setPhotoIndex, isModalOpen]);
 
   useEffect(() => {
     const handleKeyDown = (e) => {
-      if (e.key === "ArrowRight") {
-        increasePhotoIndex();
-      } else if (e.key === "ArrowLeft") {
-        decreasePhotoIndex();
+      if (!isModalOpen) {
+        if (e.key === "ArrowRight") {
+          increasePhotoIndex();
+        } else if (e.key === "ArrowLeft") {
+          decreasePhotoIndex();
+        }
       }
     };
     window.addEventListener("keydown", handleKeyDown);
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
-  }, [photoIndex, decreasePhotoIndex, increasePhotoIndex]);
+  }, [photoIndex, decreasePhotoIndex, increasePhotoIndex, isModalOpen]);
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -74,6 +86,8 @@ const ImageCarousel = ({ photos = [] }) => {
             onClose={closeModal}
             photos={photos}
             caption={photos[photoIndex]?.caption}
+            photoIndex={photoIndex}
+            setPhotoIndex={setPhotoIndex}
           />
         </>
       )}
